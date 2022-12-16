0f57a10bf9b5c65a21b18ee89b97cec6be573f37
cwang12197 <wchristina4@gmail.com>
2022-12-16 16:20:13 -0500

    added heading skeleton code

---
+++ ui/src/Heading/Heading.js
@@ -0,0 +1,12 @@
+import "./Heading.css";
+
+function Heading() {
+
+    return (
+        <div className = "heading">
+            <p> heading </p>
+        </div>
+    )
+
+}
+export default Heading;
